b34ca5b906a03629a1bb828011b445ee52c57888
shenxianpeng <xianpeng.shen@gmail.com>
2025-06-12 06:51:17 +0000

    update gen cache file name

--- lib/utils.bash
+++ lib/utils.bash
@@ -45,11 +45,12 @@ sort_versions() {
     LC_ALL=C sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-GITHUB_RELEASES_CACHE="${TMPDIR:-/tmp}/clang-tools-releases.json"
+TODAY=$(date +%Y%m%d)
+GITHUB_RELEASES_CACHE="${TMPDIR:-/tmp}/clang-tools-releases-${TODAY}.json"
 
 fetch_all_assets() {
-  # Only fetch if cache does not exist or is older than 5 minutes
-  if [ ! -f "$GITHUB_RELEASES_CACHE" ] || [ $(( $(date +%s) - $(stat -c %Y "$GITHUB_RELEASES_CACHE") )) -gt 300 ]; then
+  # Only fetch if cache does not exist
+  if [ ! -f "$GITHUB_RELEASES_CACHE" ]; then
     curl -s -H "Accept: application/vnd.github.v3+json" \
       "${curl_opts[@]}" \
       "https://api.github.com/repos/${GH_REPO}/releases" > "$GITHUB_RELEASES_CACHE"
