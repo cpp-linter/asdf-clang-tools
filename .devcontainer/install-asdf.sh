e95b0bc7875ca7f0eae370239514d5b3e20f9a5b
shenxianpeng <xianpeng.shen@gmail.com>
2025-07-31 22:58:55 +0300

    fix: update install-asdf.sh to fix lint fail

--- .devcontainer/install-asdf.sh
+++ .devcontainer/install-asdf.sh
@@ -7,7 +7,7 @@ export ASDF_VERSION=v0.18.0
 rm /tmp/asdf.tar.gz || true
 
 curl -L https://github.com/asdf-vm/asdf/releases/download/${ASDF_VERSION}/asdf-${ASDF_VERSION}-linux-amd64.tar.gz -o /tmp/asdf.tar.gz
-    
+
 mkdir -p /opt/asdf
 
 tar -xzf /tmp/asdf.tar.gz -C /opt/asdf
